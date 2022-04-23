3e2492b45f4a2d17b0ef11579bf62cfaa2960fe1
Saksham Jiaswal <sjais973@gmail.com>
2022-04-23 20:20:00 +0530

    react-app

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data ={
+    products:[
+        {
+            _id:"1",
+            name:"Nike Slim Shirt",
+            category:'Shirts',
+            image:"./images/p1.jpg",
+            price:120,
+            brand:'Nike',
+            rating:"4.5",
+            numReviews:10,
+            description:"High Quality Product"
+        },
+        {
+            _id:"2",
+            name:"Addidas Slim Shirt",
+            category:'Shirts',
+            image:"./images/p2.jpg",
+            price:120,
+            brand:'Nike',
+            rating:"4.5",
+            numReviews:10,
+            description:"High Quality Product"
+        },
+        {
+            _id:"3",
+            name:"Puma Slim Shirt",
+            category:'Shirts',
+            image:"./images/p3.jpg",
+            price:120,
+            brand:'Nike',
+            rating:"4.5",
+            numReviews:10,
+            description:"High Quality Product"
+        },
+        {
+            _id:"4",
+            name:"Lecoste Slim Shirt",
+            category:'Shirts',
+            image:"./images/p4.jpg",
+            price:156,
+            brand:'Nike',
+            rating:"4.5",
+            numReviews:10,
+            description:"High Quality Product"
+        },
+        {
+            _id:"5",
+            name:"Gucci Slim Shirt",
+            category:'Shirts',
+            image:"./images/p5.jpg",
+            price:127,
+            brand:'Nike',
+            rating:"4.5",
+            numReviews:10,
+            description:"High Quality Product"
+        },
+        {
+            _id:"6",
+            name:"D&G Slim Shirt",
+            category:'Shirts',
+            image:"./images/p6.jpg",
+            price:1200,
+            brand:'Nike',
+            rating:"4.5",
+            numReviews:10,
+            description:"High Quality Product"
+        }
+    ]
+}
+export default data ;
