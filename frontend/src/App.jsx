33debd1c3ce1128030ecb7b88c90129c6d64fb3d
Saksham Jiaswal <sjais973@gmail.com>
2022-04-26 16:05:15 +0530

    rating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import data from "./data";
+import Product from "./components/Product";
 function App() {
   return (
     <div className="grid-container">
@@ -10,41 +11,14 @@ function App() {
           </a>
         </div>
         <div className="">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div className="row center">
           {data.products.map((product, index) => (
-            <div className="card" key={product._id}>
-              <a href={`/product/${product._id}`}>
-                <img className="medium" src={product.image} alt={product.name} />
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                </div>
-                <div className="price">{product.price}</div>
-              </div>
-            </div>
+         <Product product={product} index={index}  key={index}/>
           ))}
         </div>
       </main>
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -29,7 +29,7 @@ const data ={
             image:"./images/p3.jpg",
             price:120,
             brand:'Nike',
-            rating:"4.5",
+            rating:"1.5",
             numReviews:10,
             description:"High Quality Product"
         },
@@ -40,7 +40,7 @@ const data ={
             image:"./images/p4.jpg",
             price:156,
             brand:'Nike',
-            rating:"4.5",
+            rating:"2.5",
             numReviews:10,
             description:"High Quality Product"
         },
@@ -51,7 +51,7 @@ const data ={
             image:"./images/p5.jpg",
             price:127,
             brand:'Nike',
-            rating:"4.5",
+            rating:"1",
             numReviews:10,
             description:"High Quality Product"
         },
@@ -62,7 +62,7 @@ const data ={
             image:"./images/p6.jpg",
             price:1200,
             brand:'Nike',
-            rating:"4.5",
+            rating:"3",
             numReviews:10,
             description:"High Quality Product"
         }
