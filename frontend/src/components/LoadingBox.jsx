17f1710d1f27d2bbc1e6d9faeebd75f5d8782c5e
Saksham Jiaswal <sjais973@gmail.com>
2022-04-27 22:41:17 +0530

    eslint

---
+++ frontend/src/components/LoadingBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingBox = () => {
+  return (
+    <div><i  className='fa fa-spinner fa-spin'>Loading...</i></div>
+  )
+}
+
+export default LoadingBox
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import Product from '../components/Product'
-import data from '../data'
+import React, { useState, useEffect } from "react";
+import Product from "../components/Product";
+
+import axios from "axios";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setloading(true);
+        const { data } = await axios.get("/api/products");
+        setloading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setloading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="row center">
-          {data.products.map((product, index) => (
-         <Product product={product} index={index}  key={index}/>
+    <div className="">
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product, index) => (
+            <Product product={product} index={index} key={index} />
           ))}
         </div>
-  )
-}
+      )}
+    </div>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
