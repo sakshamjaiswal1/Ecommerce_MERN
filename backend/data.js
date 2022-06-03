0e12cfda70c474158f653f05f9044cb717350974
Saksham Jiaswal <sjais973@gmail.com>
2022-06-04 00:02:48 +0530

    back

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,22 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users:[
+    {
+      name:"Saksham",
+      email:"sjais973@gmail.com",
+      password:bcrypt.hashSync("1234",8),
+      isAdmin:true,
+      
+    },
+    {
+      name:"Tannu",
+      email:"tp1999@gmail.com",
+      password:bcrypt.hashSync("1234",8),
+      isAdmin:false,
+      
+    }
+  ],
     products: [
       {
         _id: '1',
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,42 @@
-import  express from "express";
-import data from './data.js'
+import express from "express";
+import dotenv from "dotenv";
+import data from "./data.js";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
 
-const app = express()
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+mongoose.connect(
+  process.env.MONGODB_URL ||
+    "mongodb+srv://sakshamjaiswal1:789sakSHAM@cluster0.4w2j4oq.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
 
-app.get('/api/products/:id',(req,res)=>{
-    const product= data.products.find(x=>x._id===req.params.id)
-    if(product){
-        res.send(product)
-    }
-    else{
-        res.status(404).send({message:"Product Not Found"})
-    }
-})
-
-app.get('/api/products',(req,res)=>{
-    res.send(data.products)
-})
-
-app.get('/',(req,res)=>{
-    res.send('Server is ready')
-})
-const port = process.env.PORT || 5001
-app.listen(port,()=>{
-    console.log(`server is running on ${port}`)
-})
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+app.use("/api/users", userRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
